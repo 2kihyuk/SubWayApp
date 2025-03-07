52713e488e195605321ee5306c38912559550c7d
2kihyuk <kihyuk5566@naver.com>
2025-03-07 20:11:13 +0900

    API SUCCESS

--- lib/Screen/SubWay/Model/subway_model.dart
+++ lib/Screen/SubWay/Model/subway_model.dart
@@ -8,7 +8,7 @@ class ApiResponse {
   });
 
   factory ApiResponse.fromJson(Map<String, dynamic> json) {
-    var list = json['realTimeArrival'] as List;
+    var list = json['realtimeArrivalList'] as List;
     List<RealTimeArrival> arrivalList =
         list.map((i) => RealTimeArrival.fromJson(i)).toList();
 
@@ -41,8 +41,8 @@ class ErrorMessage {
       status: json['status'],
       code: json['code'],
       message: json['message'],
-      link: json['link'],
-      developerMessage: json['developerMessage'],
+      link: json['link'] ?? '',
+      developerMessage: json['developerMessage']?? '',
       total: json['total'],
     );
   }
@@ -54,19 +54,17 @@ class RealTimeArrival {
   final String trainLineNm; //- String(목적지역 - 다음역)(방화행 - 우장산방면)
   final String statnFid; // - String  이전지하철역ID
   final String statnTid; //- String 다음지하철역 ID
-  final String statNId; //- String 지하철역 ID
+  final String statnId; //- String 지하철역 ID
   final String statnNm; //- String 지하철역이름
-  final String
-      ordkey; //- String 01000방화0 도착예정열차순번 (상하행코드(1자리),순번(첫번째,두번째열차,1자리), 첫번째 도착예정 정류장 - 현재정류장(3자리),목자리 정류장,급행여부(1자리)))
-  final String barvIDt; //- String 열차도착예정시간(단위:초)
+  final String ordkey; //- String 01000방화0 도착예정열차순번 (상하행코드(1자리),순번(첫번째,두번째열차,1자리), 첫번째 도착예정 정류장 - 현재정류장(3자리),목자리 정류장,급행여부(1자리)))
+  final String barvlDt; //- String 열차도착예정시간(단위:초)
   final String btrainNo; //- String열차번호 (현재 운행하고있는 호선별 열차번호)
   final String bstatnId; //- String 종착지하철역ID
   final String bstatnNm; //- String 종학지하철역명
   final String arvlMsg2; //- String 첫번째도착메시지(도착,출발,진입등) - 화곡 전역출발
   final String arvlMsg3; //- String 두번째 도착메시지(화곡)
-  final String
-      arvlCd; //- String 도착코드 (0:진입 , 1:도착 , 2:출발 , 3:전역출발 , 4:전역진입, 5: 전역도착, 99:운행중)
-  final String istcarAt; //- String 막차여부 (1:막차, 0:아님)
+  final String arvlCd; //- String 도착코드 (0:진입 , 1:도착 , 2:출발 , 3:전역출발 , 4:전역진입, 5: 전역도착, 99:운행중)
+  final String lstcarAt; //- String 막차여부 (1:막차, 0:아님)
 
   RealTimeArrival({
       required this.subwayId,
@@ -74,17 +72,17 @@ class RealTimeArrival {
       required this.trainLineNm,
       required this.statnFid,
       required this.statnTid,
-      required this.statNId,
+      required this.statnId,
       required this.statnNm,
       required this.ordkey,
-      required this.barvIDt,
+      required this.barvlDt,
       required this.btrainNo,
       required this.bstatnId,
       required this.bstatnNm,
       required this.arvlMsg2,
       required this.arvlMsg3,
       required this.arvlCd,
-      required this.istcarAt
+      required this.lstcarAt
   });
 
   factory RealTimeArrival.fromJson(Map<String, dynamic> json) {
@@ -94,17 +92,17 @@ class RealTimeArrival {
       trainLineNm: json['trainLineNm'],
       statnFid: json['statnFid'],
       statnTid: json['statnTid'],
-      statNId: json['statNId'],
+      statnId: json['statnId'],
       statnNm: json['statnNm'],
       ordkey: json['ordkey'],
-      barvIDt: json['barvIDt'],
+      barvlDt: json['barvlDt'],
       btrainNo: json['btrainNo'],
       bstatnId: json['bstatnId'],
       bstatnNm: json['bstatnNm'],
       arvlMsg2: json['arvlMsg2'],
       arvlMsg3: json['arvlMsg3'],
       arvlCd: json['arvlCd'],
-      istcarAt: json['istcarAt'],
+      lstcarAt: json['lstcarAt'],
     );
   }
 }
--- lib/Screen/SubWay/subway_main.dart
+++ lib/Screen/SubWay/subway_main.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:privateapp/Screen/Default_layout/default_layout.dart';
+import 'package:privateapp/Screen/SubWay/Model/subway_model.dart';
 
 import '../../data/data.dart';
 
@@ -12,63 +13,103 @@ class SubwayMain extends StatefulWidget {
 }
 
 class _SubwayMainState extends State<SubwayMain> {
+  List<RealTimeArrival> arrivalInformation = [];
+
+  String selectedsubWay = '1';
+  bool isListOpen = false;
+  String selectedStation = '';
+  TextEditingController editingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final String station = "화곡";
 
-    final List<String> arrivals = [
-      "5168 - 방화행",
-      "5646 - 목동행",
-      "5167 - 우장산행",
-      "5693 - 송정행",
-    ];
-
-    final List<Map<String,dynamic>> arrivalInformation = [];
-
     return DefaultLayout(
-
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            OutlinedButton(
-                onPressed: () {
-                  getArrivalData(station);
-                },
-                child: Text('지하철 정보 받기'),
-              ),
-            Divider(),
-            Expanded(
-              flex: 2,
-              child: ListView.builder(
-                itemCount: arrivals.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Center(
-                    child: Container(
-                      child: Text(arrivals[index]),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        // DropdownButton(
+        //   value: selectedsubWay,
+        //   items: subWay
+        //       .map((items) => DropdownMenuItem<String>(
+        //             child: Text(items),
+        //             value: items,
+        //           ))
+        //       .toList(),
+        //   onChanged: (dynamic value) {
+        //     setState(() {
+        //       selectedsubWay = value;
+        //       isListOpen = true;
+        //     });
+        //   },
+        // ),
+        // if (isListOpen)
+        //   DropdownButton(
+        //     value: selectedStation,
+        //     items: items,
+        //     onChanged: (dynamic value){
+        //       selectedStation = value;
+        //     },
+        //   ),
+        TextFormField(
+          controller: editingController,
+          decoration: InputDecoration(border: OutlineInputBorder()),
+
+        ),
+        OutlinedButton(
+          onPressed: () {
+            getArrivalData(station);
+          },
+          child: Text('지하철 정보 받기'),
+        ),
+        Divider(),
+        Expanded(
+          flex: 2,
+          child: ListView.builder(
+            itemCount: arrivalInformation.length,
+            itemBuilder: (BuildContext context, int index) {
+              var minute =
+                  (int.tryParse(arrivalInformation[index].barvlDt)! / 60)
+                      .toInt();
+
+              return ListTile(
+                title: Text(
+                    '${arrivalInformation[index].subwayId == '1005' ? '5호선' : arrivalInformation[index].subwayId} - ${arrivalInformation[index].btrainNo}'),
+                subtitle: Text(
+                    '${arrivalInformation[index].trainLineNm} - ${arrivalInformation[index].bstatnNm}'),
+                trailing: Text('도착 예정 시간: ${minute}분'),
+              );
+            },
+          ),
         )
-    );
+      ],
+    ));
   }
 
   Future<void> getArrivalData(String station) async {
     final dio = Dio();
 
-    try{
-      final resp = await dio.get("http://swopenAPI.seoul.go.kr/api/subway/${APIKEY}/json/realtimeStationArrival/0/4/${station}");
-      if(resp.statusCode == 200){
-        print(resp.data);
-      }else{
+
+    print('${editingController.text}');
+    try {
+      final resp = await dio.get(
+          "http://swopenAPI.seoul.go.kr/api/subway/${APIKEY}/json/realtimeStationArrival/0/4/${station}");
+      if (resp.statusCode == 200) {
+        print('Resp.data : ${resp.data}');
+
+        ApiResponse apiResponse = ApiResponse.fromJson(resp.data);
+
+        setState(() {
+          arrivalInformation = apiResponse.realTimeArrival;
+        });
+
+        print('ArrivalInformation : ${arrivalInformation}');
+      } else {
         print("Error - SubWay-Main - getArrivalData - ${resp.statusCode}");
       }
-    }catch(e){
-        print("Catch-Error - SubWay-Main - getArrivalData - ${e}");
+    } catch (e) {
+      print("Catch-Error - SubWay-Main - getArrivalData - ${e}");
     }
   }
-
 }
